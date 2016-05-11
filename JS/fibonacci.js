9d49ed20dfbf78aca9dbc51f5715891c1895e5af
jaketarnow <jtarnow@dons.usfca.edu>
2016-05-10 17:36:48 -0700

    recursive algo fibo

---
+++ JS/fibonacci.js
@@ -0,0 +1,16 @@
+/* Recursive Algorithm Review -- Fibonacci -- */
+function fibonacci(n) {
+	if (n <= 1) {
+		return n;
+	} else {
+		return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+}
+
+function testFibo() {
+	var num = 12;
+	console.log("n is: " + num);
+	console.log("nth Fibo is: " + fibonacci(num));
+}
+
+testFibo();
