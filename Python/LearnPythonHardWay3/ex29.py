c7c237b9137bb80bd5d1b13c46373a2467fae110
jaketarnow <jtarnow@dons.usfca.edu>
2016-08-11 14:57:56 -0700

    conditionals in py

---
+++ Python/LearnPythonHardWay3/ex29.py
@@ -0,0 +1,27 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+	print "Too many cats!"
+
+if people > cats: 
+	print "Not many cats!"
+
+if people < dogs:
+	print "The world is covered in drool"
+
+if people > dogs:
+	print "HEYYYYY"
+
+dogs += 5
+
+if people >= dogs:
+	print "People are greater than or equal to dogs"
+
+if people <= dogs:
+	print "People are lteq to dogs"
+
+if people == dogs:
+	print "People are dogs"
+	
